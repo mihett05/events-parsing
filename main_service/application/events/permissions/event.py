2b7368fcd6e9d4fa0aa0c9258cb352f14f5c1b06
AlexandrVino <alexander-vin0@yandex.ru>
2025-03-27 20:45:59 +0500

    fix issues

--- main_service/application/events/permissions/event.py
+++ main_service/application/events/permissions/event.py
@@ -5,36 +5,28 @@ from domain.users.entities import User
 
 
 class EventPermissionProvider(PermissionProvider):
+    __admin_perms: set[PermissionsEnum] = {
+        PermissionsEnum.CAN_DELETE_EVENT,
+        PermissionsEnum.CAN_READ_EVENT,
+        PermissionsEnum.CAN_UPDATE_EVENT,
+    }
+    __member_perms: set[PermissionsEnum] = {
+        PermissionsEnum.CAN_READ_EVENT,
+    }
+    __public_perms: set[PermissionsEnum] = {
+        PermissionsEnum.CAN_READ_EVENT,
+    }
+
     def __init__(self, event: Event, actor: User):
         self.permissions = self.__get_perms(event, actor)
 
-    @staticmethod
-    def __get_admin_perms() -> set[PermissionsEnum]:
-        return {
-            PermissionsEnum.CAN_DELETE_EVENT,
-            PermissionsEnum.CAN_READ_EVENT,
-            PermissionsEnum.CAN_UPDATE_EVENT,
-        }
-
-    @staticmethod
-    def __get_member_perms() -> set[PermissionsEnum]:
-        return {
-            PermissionsEnum.CAN_READ_EVENT,
-        }
-
-    @staticmethod
-    def __get_public_perms() -> set[PermissionsEnum]:
-        return {
-            PermissionsEnum.CAN_READ_EVENT,
-        }
-
     def __get_perms(self, entity: Event, actor: User) -> set[PermissionsEnum]:
         if actor.id in entity.admins:
-            return self.__get_admin_perms()
+            return self.__admin_perms
         if actor.id in entity.members:
-            return self.__get_member_perms()
+            return self.__member_perms
         if entity.is_visible:
-            return self.__get_public_perms()
+            return self.__public_perms
         return set()
 
     def __call__(self) -> set[PermissionsEnum]:
