c60524bec9e14f24f1b9a70a6c40138ff48d9fa9
artem <arta90633@gmail.com>
2025-04-15 00:05:45 +0500

    added api

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/app/app.tsx';
 import { CssBaseline } from '@mui/material';
+import { Provider } from 'react-redux';
+import { store } from './shared/store/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <Provider store={store}>
+      <CssBaseline />
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
--- frontend/src/pages/CalendarPage/ui/CalendarPage.tsx
+++ frontend/src/pages/CalendarPage/ui/CalendarPage.tsx
@@ -2,15 +2,30 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { MonthCalendar } from '@widgets/MonthCalendar';
 import { parseISO } from 'date-fns';
+import { useReadAllEventsV1EventsGetQuery } from '@/shared/api/api';
+import { CircularProgress } from '@mui/material';
+// import { ApiEvent, formatEvents } from '@/shared/lib/formatEvents';
 import { mockEvents } from '@/shared/lib/mockEvents';
 
 const initialCalendarDate = parseISO('2025-03-01');
 
 export const CalendarPage: React.FC = () => {
+  const {
+    data: events,
+    error,
+    isLoading,
+  } = useReadAllEventsV1EventsGetQuery({ page: 0, pageSize: 100000 });
+  console.log(events, error, isLoading);
+
+  if (isLoading) {
+    return <CircularProgress />;
+  }
+
   return (
     <Container maxWidth="xl" sx={{ py: 2 }}>
       <Box>
         <MonthCalendar initialDate={initialCalendarDate} events={mockEvents} />
+        {/* <MonthCalendar initialDate={initialCalendarDate} events={formatEvents(events as ApiEvent[])} /> */}
       </Box>
     </Container>
   );
