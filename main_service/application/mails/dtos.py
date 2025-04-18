ecc1dd0a41ee12b444578525fcca90f6a26bed04
AlexandrVino <alexander-vin0@yandex.ru>
2025-04-18 19:16:31 +0500

    Fix code issues

--- main_service/application/mails/dtos.py
+++ main_service/application/mails/dtos.py
@@ -9,6 +9,3 @@ class UpdateMailDto:
     id: int
     state: MailStateEnum
     event_id: int | None = None
-    retry_after: datetime = field(
-        default_factory=lambda: datetime.now() + timedelta(minutes=30)
-    )
