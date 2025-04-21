3eacc67658629934d59c8b508eb5c764dd717887
mihett05 <mihett05@yandex.ru>
2025-04-22 01:23:54 +0500

    Added feed: components, slices, api

--- frontend/src/shared/store/store.ts
+++ frontend/src/shared/store/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { api } from '../api/api';
-
+import eventsReducer from '@features/events/slice';
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
+    events: eventsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
 });
