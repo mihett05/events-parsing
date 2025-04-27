38417851244a960159723823c11197c1a671d655
AlexandrVino <alexander-vin0@yandex.ru>
2025-04-27 15:14:48 +0500

    Move tests to tests/unittests, add basic conftest for integration test

--- main_service/infrastructure/tests/unittests/organizations/usecases/test_readall.py
+++ main_service/infrastructure/tests/unittests/organizations/usecases/test_readall.py
@@ -3,16 +3,13 @@ import pytest
 from application.organizations.usecases import ReadAllOrganizationUseCase
 from domain.organizations.dtos import ReadOrganizationsDto
 from domain.organizations.entities import Organization
-from infrastructure.tests.organizations.usecases.conftest import (
-    readall_organization_usecase,
-)
 
 
 @pytest.mark.asyncio
 async def test_readall(
-    readall_organization_usecase: ReadAllOrganizationUseCase,
-    readall_organizations_dto: ReadOrganizationsDto,
-    create_organization: Organization,
+        readall_organization_usecase: ReadAllOrganizationUseCase,
+        readall_organizations_dto: ReadOrganizationsDto,
+        create_organization: Organization,
 ):
     organizations = await readall_organization_usecase(
         readall_organizations_dto
@@ -23,8 +20,8 @@ async def test_readall(
 
 @pytest.mark.asyncio
 async def test_read_all_empty(
-    readall_organization_usecase: ReadAllOrganizationUseCase,
-    readall_organizations_dto: ReadOrganizationsDto,
+        readall_organization_usecase: ReadAllOrganizationUseCase,
+        readall_organizations_dto: ReadOrganizationsDto,
 ):
     readall_organizations_dto.page = 100
     organizations = await readall_organization_usecase(
