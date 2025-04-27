2a912d080dd8bdbe09148113d1bfa35d13955657
AlexandrVino <alexander-vin0@yandex.ru>
2025-04-27 20:55:55 +0500

    Fix issue with interation tests

--- main_service/infrastructure/tests/integration_tests/conftest.py
+++ main_service/infrastructure/tests/integration_tests/conftest.py
@@ -13,29 +13,29 @@ from infrastructure.mocks.providers.container import (
 )
 
 
-@pytest_asyncio.fixture(scope="session")
+@pytest_asyncio.fixture
 async def container() -> Iterable[AsyncContainer]:
     container = create_integration_test_container()
     yield container
     await container.close()
 
 
-@pytest_asyncio.fixture(scope="session")
+@pytest_asyncio.fixture
 async def config() -> Config:
     return get_config()
 
 
-@pytest_asyncio.fixture(scope="session", autouse=True)
+@pytest_asyncio.fixture
 async def get_app(container: AsyncContainer, config: Config):
     app = create_app(container, config)
     yield app
     shutil.rmtree("static")
 
 
-@pytest_asyncio.fixture(scope="session", autouse=True)
+@pytest_asyncio.fixture(autouse=True)
 async def get_test_client(get_app: FastAPI) -> AsyncClient:
     transport = ASGITransport(app=get_app)
     async with AsyncClient(
-        transport=transport, base_url="http://testserver"
+            transport=transport, base_url="http://testserver"
     ) as client:
         yield client
