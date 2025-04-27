43f851624f4c3a41aaf1cc876e56543c5109b295
AlexandrVino <alexander-vin0@yandex.ru>
2025-04-27 15:17:13 +0500

    Move tests to tests/unittests, add basic conftest for integration test

---
+++ main_service/infrastructure/tests/integration_tests/conftest.py
@@ -0,0 +1,37 @@
+import shutil
+from typing import Iterable
+
+import pytest_asyncio
+from dishka import AsyncContainer
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from infrastructure.api.app import create_app
+from infrastructure.config import Config
+from infrastructure.mocks.providers.container import create_test_container
+
+
+@pytest_asyncio.fixture(scope='package')
+async def container() -> Iterable[AsyncContainer]:
+    container = create_test_container()
+    yield container
+    await container.close()
+
+
+@pytest_asyncio.fixture(scope='package')
+async def config() -> Iterable[AsyncContainer]:
+    container = create_test_container()
+    yield container
+    await container.close()
+
+
+@pytest_asyncio.fixture(scope='package', autouse=True)
+async def get_app(container: AsyncContainer, config: Config):
+    app = create_app(container, config)
+    yield app
+    shutil.rmtree("static")
+
+
+@pytest_asyncio.fixture(scope='package', autouse=True)
+async def get_test_client(app: FastAPI) -> TestClient:
+    yield TestClient(app)
