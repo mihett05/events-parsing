4c620c3b9e2afc7f4a3a72df4c6d17ca1c262035
AlexandrVino <alexander-vin0@yandex.ru>
2025-04-28 09:47:53 +0500

    Cleanup

--- main_service/infrastructure/database/alembic/versions/2025_4_28__9_34_25__eb47cacc5420_add_mails_model.py
+++ main_service/infrastructure/database/alembic/versions/2025_4_28__9_34_25__eb47cacc5420_add_mails_model.py
@@ -8,9 +8,6 @@ Create Date: 2025-04-28 09:34:25.456319
 
 from typing import Sequence, Union
 
-import sqlalchemy as sa
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision: str = "eb47cacc5420"
 down_revision: Union[str, None] = "53ffba81937f"
--- main_service/infrastructure/database/alembic/versions/2025_4_28__9_34_59__fcb766c9c616_add_attachmetns_model.py
+++ main_service/infrastructure/database/alembic/versions/2025_4_28__9_34_59__fcb766c9c616_add_attachmetns_model.py
@@ -8,9 +8,6 @@ Create Date: 2025-04-28 09:34:59.961988
 
 from typing import Sequence, Union
 
-import sqlalchemy as sa
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision: str = "fcb766c9c616"
 down_revision: Union[str, None] = "eb47cacc5420"
