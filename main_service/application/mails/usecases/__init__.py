6c15e72537bc49c418465952085b02eab80bee35
AlexandrVino <alexander-vin0@yandex.ru>
2025-04-29 12:33:10 +0500

    Fix

--- main_service/application/mails/usecases/__init__.py
+++ main_service/application/mails/usecases/__init__.py
@@ -1,11 +1,9 @@
-from .create import CreateMailUseCase
 from .create_many import CreateMailsUseCase
 from .read import ReadMailUseCase
 from .read_unprocessed import ReadUnprocessedMailUseCase
 from .update import UpdateMailUseCase
 
 __all__ = [
-    "CreateMailUseCase",
     "ReadMailUseCase",
     "ReadUnprocessedMailUseCase",
     "UpdateMailUseCase",
