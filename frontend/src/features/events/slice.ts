108ce090940c87f06314b7d6b5f0adbd5363a9c1
mihett05 <mihett05@yandex.ru>
2025-04-29 13:16:17 +0500

    Merged slices, added filters state

--- frontend/src/features/events/slice.ts
+++ frontend/src/features/events/slice.ts
@@ -5,12 +5,27 @@ import { api } from '@shared/api/api';
 
 export const eventsAdapter = createEntityAdapter<CalendarEvent>();
 
+type FilterState = {
+  start: Date | null;
+  end: Date | null;
+  type: string | null;
+  format: string | null;
+};
+
+type FilterVariants = {
+  types: string[];
+  formats: string[];
+};
+
 interface EventsState {
   events: ReturnType<typeof eventsAdapter.getInitialState>;
   isLoading: boolean;
   error: string | null;
   page: number;
   pageSize: number;
+  selectedDate: Date;
+  filters: FilterState;
+  filterVariants: FilterVariants;
 }
 
 const initialState: EventsState = {
@@ -19,6 +34,17 @@ const initialState: EventsState = {
   pageSize: 20,
   isLoading: false,
   error: null,
+  selectedDate: new Date(),
+  filters: {
+    start: null,
+    end: null,
+    type: null,
+    format: null,
+  },
+  filterVariants: {
+    formats: [],
+    types: [],
+  },
 };
 
 const eventsSlice = createSlice({
@@ -32,22 +58,67 @@ const eventsSlice = createSlice({
       if (state.isLoading) return;
       state.page++;
     },
+    setSelectedDate: (state, action: PayloadAction<Date>) => {
+      state.selectedDate = action.payload;
+    },
+    setFilters: (state, filter: PayloadAction<FilterState>) => {
+      state.filters = filter.payload;
+    },
   },
   extraReducers: (builder) => {
     builder
-      .addMatcher(api.endpoints.readAllEventsV1EventsFeedGet.matchFulfilled, (state, { payload }) => {
-        state.isLoading = false;
-        eventsAdapter.upsertMany(state.events, payload.map(mapEventToCalendarEvent));
-      })
+      .addMatcher(
+        api.endpoints.readAllEventsV1EventsFeedGet.matchFulfilled,
+        (state, { payload }) => {
+          state.isLoading = false;
+          eventsAdapter.upsertMany(state.events, payload.map(mapEventToCalendarEvent));
+          updateFilterVariants(state);
+        },
+      )
       .addMatcher(api.endpoints.readAllEventsV1EventsFeedGet.matchPending, (state) => {
         state.isLoading = true;
       })
       .addMatcher(api.endpoints.readAllEventsV1EventsFeedGet.matchRejected, (state, { error }) => {
         state.isLoading = false;
         state.error = error.message ?? null;
-      });
+      })
+      .addMatcher(api.endpoints.readAllEventsV1EventsCalendarGet.matchPending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addMatcher(
+        api.endpoints.readAllEventsV1EventsCalendarGet.matchFulfilled,
+        (state, { payload }) => {
+          state.isLoading = false;
+          eventsAdapter.setAll(state.events, payload.map(mapEventToCalendarEvent));
+          updateFilterVariants(state);
+        },
+      )
+      .addMatcher(
+        api.endpoints.readAllEventsV1EventsCalendarGet.matchRejected,
+        (state, { error }) => {
+          state.isLoading = false;
+          state.error = error.message ?? 'Ошибка загрузки календаря';
+        },
+      );
   },
 });
 
-export const { upsertEvents, incrementPage } = eventsSlice.actions;
+const updateFilterVariants = (state: EventsState) => {
+  const events = eventsAdapter.getSelectors().selectAll(state.events);
+  const formats = new Set();
+  const types = new Set();
+
+  events.forEach((event) => {
+    formats.add(event.format);
+    types.add(event.type);
+  });
+
+  state.filterVariants = {
+    formats: Array.from(formats.values()) as string[],
+    types: Array.from(types.values()) as string[],
+  };
+};
+
+export const { upsertEvents, incrementPage, setSelectedDate } = eventsSlice.actions;
 export default eventsSlice.reducer;
