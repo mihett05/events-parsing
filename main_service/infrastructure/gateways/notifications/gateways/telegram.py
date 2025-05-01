5c482d27ab42383f39deb631a3c8dde2621066e3
crazyhamster310 <ruhlyadev.alex@yandex.ru>
2025-05-01 17:28:18 +0500

    Added format in message

--- main_service/infrastructure/gateways/notifications/gateways/telegram.py
+++ main_service/infrastructure/gateways/notifications/gateways/telegram.py
@@ -1,6 +1,8 @@
 from aiogram import Bot
+from aiogram.enums import ParseMode
 from application.notifications.gateway import NotificationGateway
 from domain.notifications.entities import Notification
+from domain.notifications.enums import NotificationFormatEnum
 from domain.notifications.exceptions import FailedSendNotificationError
 from domain.users.entities import User
 
@@ -8,6 +10,16 @@ from domain.users.entities import User
 class NotificationTelegramGateway(NotificationGateway):
     def __init__(self, bot: Bot):
         self.__bot = bot
+        self.__mapper = {
+            NotificationFormatEnum.RAW_TEXT: None,
+            NotificationFormatEnum.HTML: ParseMode.HTML,
+            NotificationFormatEnum.MARKDOWN: ParseMode.MARKDOWN,
+        }
+
+    def __map_parse_mode(
+        self, format_: NotificationFormatEnum
+    ) -> ParseMode | None:
+        return self.__mapper[format_]
 
     async def send(
         self, notification: Notification, recipient: User
@@ -16,7 +28,9 @@ class NotificationTelegramGateway(NotificationGateway):
             raise FailedSendNotificationError
         try:
             await self.__bot.send_message(
-                recipient.telegram_id, notification.text
+                chat_id=recipient.telegram_id,
+                text=notification.text,
+                parse_mode=self.__map_parse_mode(notification.format),
             )
             return notification
         except Exception:
