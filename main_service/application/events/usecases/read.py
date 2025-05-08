b30ccf98f21440f03b94ec21f82eb1472fc70d1e
glukvseti <vasilygrishkov@gmail.com>
2025-05-08 12:32:37 +0500

    changed read perms for events

--- main_service/application/events/usecases/read.py
+++ main_service/application/events/usecases/read.py
@@ -14,10 +14,4 @@ class ReadEventUseCase:
         self.__builder = builder
 
     async def __call__(self, event_id: int) -> Event:
-        event = await self.__repository.read(event_id)
-        self.__builder.providers(
-            EventPermissionProvider(event.organization_id, None)
-        ).add(
-            PermissionsEnum.CAN_READ_EVENT,
-        ).apply()
-        return event
+        return await self.__repository.read(event_id)
