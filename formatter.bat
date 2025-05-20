d031c6c0a322818c171cd985b67112cdf8cc0b15
mihett05 <mihett05@yandex.ru>
2025-05-20 12:31:43 +0500

    Fixed lock

--- formatter.bat
+++ formatter.bat
@@ -4,7 +4,7 @@ ruff --config ruff.toml check --select I --fix .
 echo.
 
 echo Running ruff format...
-ruff --config ruff.toml format --line-length 88 .
+ruff --config ruff.toml format --line-length 80 .
 echo.
 
 echo Running ruff format for integration tests...
