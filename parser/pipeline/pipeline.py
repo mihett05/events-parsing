26447a06807dcf2302516052e0ab50bc61d737af
mihett05 <mihett05@yandex.ru>
2025-05-27 01:07:15 +0500

    Updated ocr, pipeline and added dockerfile

--- parser/pipeline/pipeline.py
+++ parser/pipeline/pipeline.py
@@ -1,12 +1,26 @@
+import os
+from urllib.parse import urlparse
+
 from models import EventInfoModel, MailModel
 
 from .classification import is_text_relates_to_topics, topic_descriptions
 from .extraction import extract_features
+from .ocr import process_links
 
 
 def pipeline(mail: MailModel) -> EventInfoModel | None:
     text = f"Тема письма: {mail.theme}\nОтправитель: {mail.sender}\nДата: {mail.received_date}\n\n{mail.raw_content.decode('utf-8')}"
-    # TODO: добавить текст из документов
+    links = {
+        os.path.basename(urlparse(attachment).path): attachment
+        for attachment in mail.attachments
+    }
+    files = process_links(links)
+
+    if files:
+        text += "Приложения:\n\n"
+
+    for filename, content in files.items():
+        text += f"{filename}: {content}\n\n"
 
     if not is_text_relates_to_topics(text, topic_descriptions):
         return None
