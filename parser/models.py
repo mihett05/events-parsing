94053d9fb99c4eff4b4b0b15c1ff5c4aa9e9825d
mihett05 <mihett05@yandex.ru>
2025-05-27 12:58:02 +0500

    Fixed import error in parser

--- parser/models.py
+++ parser/models.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, date
 from enum import Enum
 
 from pydantic import BaseModel, ConfigDict
@@ -50,21 +50,3 @@ class EventInfoModel(CamelModel):
     format: EventFormatEnum
     location: str | None
     organization_name: str | None
-
-
-@dataclass
-class Event:
-    title: str
-    location: str | None
-    start_date: datetime
-
-    type: EventTypeEnum = EventTypeEnum.OTHER
-    format: EventFormatEnum = EventFormatEnum.OTHER
-
-    id: int | None = None
-    is_visible: bool = True
-    description: str | None = None
-
-    end_date: datetime | None = None
-    created_at: datetime | None = None
-    end_registration: datetime | None = None
